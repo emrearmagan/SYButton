175236c8b27784e43d6014e489c23f438a0732ad
emrearmagan <emrearmagan.dev@gmail.de>
2024-08-24 20:47:07 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -6,14 +6,20 @@ import PackageDescription
 let package = Package(
     name: "SYButton",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
     ],
     products: [
         .library(name: "SYButton", targets: ["SYButton"]),
     ],
     targets: [
-        .target(name: "SYButton", path: "Sources"),
+        .target(
+            name: "SYButton",
+            dependencies: [],
+            path: "Sources"
+        ),
+
         .testTarget(name: "SYButtonTests", dependencies: ["SYButton"]),
+
     ],
     swiftLanguageVersions: [.v5]
 )
